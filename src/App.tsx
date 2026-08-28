6554fb14b669492f997404bb40709bbd3ad6f84c
tfrvld <tfrvld@gmail.com>
2026-08-28 17:17:00 +0700

    menambahkan section certificate

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from "./sections/hero";
 import About from "./sections/about";
 import Services from "./sections/services";
 import Projects from "./sections/projects";
+import Certificates from "./sections/certificates";
 import Faqs from "./sections/faqs";
 import Contact from "./sections/contacts";
 
@@ -11,8 +12,8 @@ function App() {
       <div className="absolute inset-0 z-0 pointer-events-none">
         <div className="absolute top-[100px] md:-top-[400px] left-2/3 -translate-x-1/2 w-[400px] h-[400px] md:w-[900px] md:h-[900px] rounded-full bg-secondary blur-[160px]" />
         <div className="absolute top-[150vh] md:top-[120vh] left-1/2 -translate-x-1/2 w-[300px] h-[900px] md:w-[700px] md:h-[700px] rounded-full bg-secondary/70 blur-[160px]" />
-        <div className="absolute top-[400vh] md:top-[300vh] -left-40 w-[400px] h-[400px] md:w-[700px] md:h-[700px] rounded-full bg-secondary/60 blur-[160px]" />
-        <div className=" absolute top-[500vh] md:top-[350vh] lg:top-[380vh] -right-40 w-[400px] h-[400px] md:w-[700px] md:h-[700px] rounded-full bg-secondary/60 blur-[160px]" />
+        <div className="absolute top-[400vh] md:top-[240vh] -left-40 w-[400px] h-[400px] md:w-[700px] md:h-[700px] rounded-full bg-secondary/60 blur-[160px]" />
+        <div className=" absolute top-[490vh] md:top-[280vh] lg:top-[350vh] -right-40 w-[400px] h-[400px] md:w-[700px] md:h-[700px] rounded-full bg-secondary/60 blur-[160px]" />
       </div>
 
       <div className="relative z-10">
@@ -20,6 +21,7 @@ function App() {
         <About />
         <Services />
         <Projects />
+        <Certificates />
         <Faqs />
         <Contact />
       </div>
---
+++ src/sections/certificates.tsx
@@ -0,0 +1,76 @@
+import c1 from "../assets/sertifikat-aws.png";
+import c2 from "../assets/sertifikat-business.png";
+import c3 from "../assets/sertifikat-spec-driven.png";
+import c4 from "../assets/sertifikat-data-science.png";
+import c5 from "../assets/sertifikat-diskominfo.png";
+
+function Certificates() {
+  return (
+    <div
+      id="faqs"
+      className="w-full font-outfit mb-[2vh] lg:mb-[5vh] scroll-mt-10"
+    >
+      <h1 className="text-white text-center text-[20px] sm:text-[30px] md:text-[38px] lg:text-[43px] pt-[4vh]">
+        Certificate and License
+      </h1>
+      <h2 className="text-white text-center text-[10px] sm:text-[16px] md:text-[20px] lg:text-[20px] font-inter">
+        Answer to common question about my work and process
+      </h2>
+      <div className="flex overflow-x-scroll bg-linear-to-b from-primary from-0% via-secondary via-50% to-primary p-10 gap-5">
+        <div className="w-[340px] h-[240px] sm:w-[340px] sm:h-[260px] md:w-[340px] md:h-[260px] lg:w-[450px] lg:h-[360px] bg-whitebg/10 rounded-[10px] backdrop-blur-md inset-shadow-white/50 inset-shadow-xs flex justify-center items-center flex-col shrink-0">
+          <img
+            src={c1}
+            alt=""
+            className="h-[200px] w-[300px] lg:w-[400px] lg:h-[300px]"
+          />
+          <p className="text-white mt-1 text-[12px] lg:text-[16px]">
+            Dicoding: Belajar Dasar Cloud dan Gen AI di AWS
+          </p>
+        </div>
+
+        <div className="w-[340px] h-[240px] sm:w-[340px] sm:h-[260px] md:w-[340px] md:h-[260px] lg:w-[450px] lg:h-[360px] bg-whitebg/10 rounded-[10px] backdrop-blur-md inset-shadow-white/50 inset-shadow-xs flex justify-center items-center flex-col shrink-0">
+          <img
+            src={c3}
+            alt=""
+            className="h-[200px] w-[300px] lg:w-[400px] lg:h-[300px]"
+          />
+          <p className="text-white mt-1 text-[12px] lg:text-[16px]">
+            Dicoding: Spec-Driven Development dengan Kiro
+          </p>
+        </div>
+        <div className="w-[340px] h-[240px] sm:w-[340px] sm:h-[260px] md:w-[340px] md:h-[260px] lg:w-[450px] lg:h-[360px] bg-whitebg/10 rounded-[10px] backdrop-blur-md inset-shadow-white/50 inset-shadow-xs flex justify-center items-center flex-col shrink-0">
+          <img
+            src={c4}
+            alt=""
+            className="h-[200px] w-[300px] lg:w-[400px] lg:h-[300px]"
+          />
+          <p className="text-white mt-1 text-[12px] lg:text-[16px]">
+            Dicoding: Belajar Penerapan Data Science
+          </p>
+        </div>
+        <div className="w-[340px] h-[240px] sm:w-[340px] sm:h-[260px] md:w-[340px] md:h-[260px] lg:w-[450px] lg:h-[360px] bg-whitebg/10 rounded-[10px] backdrop-blur-md inset-shadow-white/50 inset-shadow-xs flex justify-center items-center flex-col shrink-0">
+          <img
+            src={c2}
+            alt=""
+            className="h-[200px] w-[300px] lg:w-[400px] lg:h-[300px]"
+          />
+          <p className="text-white mt-1 text-[12px] lg:text-[16px]">
+            Business Plan National Participant
+          </p>
+        </div>
+        <div className="w-[340px] h-[240px] sm:w-[340px] sm:h-[260px] md:w-[340px] md:h-[260px] lg:w-[450px] lg:h-[360px] bg-whitebg/10 rounded-[10px] backdrop-blur-md inset-shadow-white/50 inset-shadow-xs flex justify-center items-center flex-col shrink-0">
+          <img
+            src={c5}
+            alt=""
+            className="h-[200px] w-[300px] lg:w-[400px] lg:h-[300px]"
+          />
+          <p className="text-white mt-1 text-[12px] lg:text-[16px]">
+            Diskominfo Internship Program
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Certificates;
